20945bfd81d0a8acd47fc0961e818a0636427977
Scott Mitchell <scott.a.mitchell1@hotmail.com>
2022-04-13 20:04:03 +0800

    few more tweaks

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -3,12 +3,12 @@ import { clothesImages, otherImages } from "../data/img-data";
 
 const Credits = () => {
   return (
-    <div className="text-center mt-20 text-md px-5">
+    <div className="text-center mt-20 text-md px-5 mb-8">
       <div className="py-4">
         <h1 className="font-heading text-4xl">Credits</h1>
         <div className="h-[2px] bg-green-200 w-1/4 m-[0_auto]"></div>
       </div>
-      <p className="text-lg">
+      <p className="md:text-lg">
         This is to give credit to all the photographers for their images from{" "}
         <a
           href="https://unsplash.com"
@@ -21,10 +21,10 @@ const Credits = () => {
         .
       </p>
       <div>
-        <h1 className="text-3xl font-heading py-4">Clothes Images</h1>
+        <h1 className="text-3xl font-heading py-4 mt-4">Clothes Images</h1>
         <div>
           <h1 className="font-heading text-2xl py-2">T-shirts</h1>
-          <ul className="flex justify-center">
+          <ul className="md:flex justify-center">
             {clothesImages
               .filter((item) => item.cat === "t-shirt")
               .map((item, index) => {
@@ -45,8 +45,8 @@ const Credits = () => {
           </ul>
         </div>
         <div>
-          <h1 className="font-heading text-2xl py-2">Jumpers:</h1>
-          <ul className="flex justify-center">
+          <h1 className="font-heading text-2xl py-2 mt-4">Jumpers:</h1>
+          <ul className="md:flex justify-center">
             {clothesImages
               .filter((item) => item.cat === "jumper")
               .map((item, index) => {
@@ -67,8 +67,8 @@ const Credits = () => {
           </ul>
         </div>
         <div>
-          <h1 className="font-heading text-2xl py-2">Shorts</h1>
-          <ul className="flex justify-center">
+          <h1 className="font-heading text-2xl py-2 mt-4">Shorts</h1>
+          <ul className="md:flex justify-center">
             {clothesImages
               .filter((item) => item.cat === "shorts")
               .map((item, index) => {
@@ -89,8 +89,8 @@ const Credits = () => {
           </ul>
         </div>
         <div>
-          <h1 className="font-heading text-2xl py-2">Jeans</h1>
-          <ul className="flex justify-center">
+          <h1 className="font-heading text-2xl py-2 mt-4">Jeans</h1>
+          <ul className="md:flex justify-center">
             {clothesImages
               .filter((item) => item.cat === "jeans")
               .map((item, index) => {
@@ -110,9 +110,9 @@ const Credits = () => {
               })}
           </ul>
         </div>
-        <h1 className="text-3xl font-heading py-6">Other Images</h1>
+        <h1 className="text-3xl font-heading py-6 mt-4">Other Images</h1>
         <div>
-          <ul className="flex justify-center">
+          <ul className="md:flex justify-center">
             {otherImages.map((item, index) => {
               const { by, unsplahSrc } = item;
               return (
